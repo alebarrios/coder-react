7e71c1b86972cf960d9651a7746d216852f9415b
Ale Barrios <al.barrios88@gmail.com>
2023-07-16 20:36:43 -0300

    Crear useFetch para leer products.json en ItemListContainer

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import useFetch from './useFetch';
 
 const ItemListContainer = (props) => {
+  const [products] = useFetch('../src/data/products.json');
+
   return (
     <div className='bg-orange-600 text-center text-4xl text-slate-200 m-4 p-4 max-w-[80%] mx-auto rounded-xl'>{props.greeting}</div>
   );
