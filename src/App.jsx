ed86ae2e2667f1917586e76b3979917404ed98ae
Ale Barrios <al.barrios88@gmail.com>
2023-07-16 21:46:53 -0300

    Mapear productos del Json en el DOM

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import Navbar from './components/Navbar';
 import ItemListContainer from './components/ItemListContainer';
 
 function App() {
-  const greeting = "Hola, bienvenid@ a mi tienda!";
   return (
     <>
       <Navbar />
-      <ItemListContainer greeting={greeting}/>
+      <ItemListContainer/>
     </>
   );
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import useFetch from './useFetch';
+import Item from './Item';
 
-const ItemListContainer = (props) => {
-  const [products] = useFetch('../src/data/products.json');
+const ItemListContainer = () => {
+  const [data] = useFetch('../src/data/products.json');
 
   return (
-    <div className='bg-orange-600 text-center text-4xl text-slate-200 m-4 p-4 max-w-[80%] mx-auto rounded-xl'>{props.greeting}</div>
+    <div className='bg-orange-600 text-center text-4xl text-slate-200 m-4 p-4 max-w-[80%] mx-auto rounded-xl'>
+      {
+        data && data.products.map((product) => {
+          return (
+            <Item key={product.id} {...product} />
+          );
+        })
+      }
+
+    </div>
   );
 };
 
