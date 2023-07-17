6a362106bf3f7dd96aae8ed6d70f188f7d291203
Ale Barrios <al.barrios88@gmail.com>
2023-07-16 22:12:28 -0300

    Instalar react-router-dom y config basica

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import ItemListContainer from './components/ItemListContainer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
       <ItemListContainer/>
-    </>
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/category/:categoryId" element={<ItemListContainer />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
