15c83f08f02d7013433dd05bc62f298aa7b3be77
Ale Barrios <al.barrios88@gmail.com>
2023-07-16 21:45:23 -0300

    Agregar Item.js basico

---
+++ src/components/Item.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Item = ({id, name, price, image, description, category, quantity}) => {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>{id}</p>
+      <p>{price}</p>
+      <p>{category}</p>
+      <p>{quantity}</p>
+      <img src={image} alt={name} />
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default Item;
