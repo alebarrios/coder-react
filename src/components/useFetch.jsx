3b4b7c1c478abaf671b50ee607e43ec024de6277
Ale Barrios <al.barrios88@gmail.com>
2023-07-16 21:45:58 -0300

    Agregar async en useFetch

--- src/components/useFetch.jsx
+++ src/components/useFetch.jsx
@@ -4,9 +4,12 @@ const useFetch = (url) => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch(url)
-        .then((res) => res.json())
-        .then((data) => setData(data));
+    const fetchData = async () => {
+      fetch(url)
+          .then((res) => res.json())
+          .then((data) => setData(data));
+    };
+    fetchData();
   }, [url]);
 
   return [data];
