ddc690dea1358677f6ddf3ee19feb8a48392b677
Ale Barrios <al.barrios88@gmail.com>
2023-07-19 20:16:53 -0300

    Agregar ItemCount en el ItemDetail

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -12,6 +12,7 @@ const ItemDetail = ({ item }) => {
         <a onClick={() => alert("El producto " + item.name + " se agregará al carrito en la próxima entrega")}
           href="#" className="md:max-w-[10rem] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300
           font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Agregar al Carrito</a>
+        <ItemCount stockItems={item.quantity}/>
       </div>
     </div>
   );
