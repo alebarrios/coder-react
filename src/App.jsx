fe49b465d8540fa4b6df6251f4574365c0c49533
Ale Barrios <al.barrios88@gmail.com>
2023-08-06 19:23:16 -0300

    Agregar Cart. Agregar ruteo desde CartWidget a /cart

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ItemDetailContainer from './components/ItemDetailContainer';
 import Error404 from './components/Error404';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
+import Cart from './components/Cart';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<ItemListContainer />} />
           <Route path="/category/:categoryId" element={<ItemListContainer />} />
           <Route path="/item/:id" element={<ItemDetailContainer />} />
+          <Route path="/cart" element={<Cart />} />
           <Route path="*" element={<Error404 />} />
         </Routes>
       </BrowserRouter>
--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,15 +1,19 @@
 import { useContext } from "react";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 import { CartContext } from "../context/CartContext";
+import { Link } from "react-router-dom";
 
 const CartWidget = () => {
   const { getTotalItems } = useContext(CartContext);
 
   return (
-    <div className="relative">
-      <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
-      <span className="absolute top-3 left-4 p-1 text-xs">{ getTotalItems() }</span>
-    </div>);
+    <Link to={"/cart"} >
+      <div className="relative">
+        <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
+        <span className="absolute top-3 left-4 p-1 text-xs">{ getTotalItems() }</span>
+      </div>
+    </Link>
+  );
 };
 
 export default CartWidget;
