a30bb902225637b465a38f18556f897dd1edb810
Ale Barrios <al.barrios88@gmail.com>
2023-08-06 19:16:26 -0300

    Actualizar contador en CartWidget con UseContext

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import { useContext } from "react";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+import { CartContext } from "../context/CartContext";
 
 const CartWidget = () => {
+  const { getTotalItems } = useContext(CartContext);
+
   return (
     <div className="relative">
       <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
-      <span className="absolute top-3 left-4 p-1 text-xs">0</span>
+      <span className="absolute top-3 left-4 p-1 text-xs">{ getTotalItems() }</span>
     </div>);
 };
 
