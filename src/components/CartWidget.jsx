f329193f1c0f11b6f4919a6d2bbd7673f57a40bf
Ale Barrios <al.barrios88@gmail.com>
2023-08-11 01:25:14 -0300

    Estilar CartWidget

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -10,7 +10,7 @@ const CartWidget = () => {
     <Link to={"/cart"} >
       <div className="relative">
         <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
-        <span className="absolute top-3 left-4 p-1 text-xs">{ getTotalItems() }</span>
+        { getTotalItems() > 0 && <div className="absolute top-3 left-4 p-1 text-xs text-white bg-red-600 rounded-full h-[20px] w-[20px] leading-[10px] text-center">{ getTotalItems() }</div>}
       </div>
     </Link>
   );
